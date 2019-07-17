d4eb8dfbceaa7c833fd406b3be550da85186acc9
SerGolub89 <k3210389gsi@gmail.com>
2019-07-17 13:52:09 +0300

    Updates

---
+++ precache-manifest.5a9d073dd1d3affd538a5720a4fe4466.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "518276135c6c74a048be4bae2b658fa8",
+    "url": "/react_static-list-of-posts/index.html"
+  },
+  {
+    "revision": "d7cb79a4f8259ef5f1b6",
+    "url": "/react_static-list-of-posts/static/css/main.71241b78.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/react_static-list-of-posts/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "d7cb79a4f8259ef5f1b6",
+    "url": "/react_static-list-of-posts/static/js/main.4cc8c3ad.chunk.js"
+  },
+  {
+    "revision": "3fdea9cdc8e2a1e9a77e",
+    "url": "/react_static-list-of-posts/static/js/runtime~main.c48bb6e1.js"
+  }
+]);
